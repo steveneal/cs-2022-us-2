3de83ae3ee5d04111a6451cb1a21f8a016b64e05
Stephen Welsh <stephen_welsh@icloud.com>
2022-07-21 14:16:12 -0400

    Merge remote-tracking branch 'origin/main' into InstrumentAndEntity
    # Conflicts:
    #	src/main/java/com/cs/rfq/decorator/RfqProcessor.java
    #	src/main/java/com/cs/rfq/decorator/extractors/RfqMetadataFieldNames.java
    #	src/test/resources/trades/trades.json
    #	target/classes/com/cs/rfq/decorator/RfqProcessor.class
    #	target/classes/com/cs/rfq/decorator/extractors/RfqMetadataFieldNames.class
    #	target/test-classes/trades/trades.json

---
+++ src/main/java/com/cs/rfq/decorator/KafkaProcessor.java
@@ -0,0 +1,53 @@
+package com.cs.rfq.decorator;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KafkaProcessor {
+    private static final String KAFKA_SERVER = "localhost:9092";
+    private static final String RFQ_STREAM_ID = "rfq_stream_group_id";
+    private static final String TOPIC_NAME = "rfq";
+    private final JavaStreamingContext streamingContext;
+    private final SparkSession session;
+
+    public KafkaProcessor(SparkSession session, JavaStreamingContext streamingContext){
+        this.streamingContext = streamingContext;
+        this.session = session;
+    }
+
+    JavaDStream<String> initRfqStream(){
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, RFQ_STREAM_ID);
+        kafkaParams.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        kafkaParams.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        Collection<String> topics = Arrays.asList(TOPIC_NAME);
+
+        JavaInputDStream<ConsumerRecord<String, String>> records =
+                KafkaUtils.createDirectStream(
+                        streamingContext,
+                        LocationStrategies.PreferConsistent(),
+                        ConsumerStrategies.Subscribe(topics, kafkaParams));
+
+        JavaDStream<String> rfqStream = records.map(record -> record.value());
+
+        return rfqStream;
+
+    }
+}
--- src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
+++ src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
@@ -1,9 +1,22 @@
 package com.cs.rfq.decorator;
 
+import io.netty.handler.codec.string.StringDecoder;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class RfqDecoratorMain {
 
@@ -21,9 +34,23 @@ public class RfqDecoratorMain {
         //TODO: create a Spark session
         SparkSession session = SparkSession.builder().config(conf).getOrCreate();
 
+
+        KafkaProcessor processor = new KafkaProcessor(session, jssc);
+
+        JavaDStream<String> stream = processor.initRfqStream();
+
+        //.map(Rfq::fromJson)
+        stream.foreachRDD(rdd -> {
+                    rdd.collect().stream().forEach(System.out::println);
+                }
+        );
+
         //TODO: create a new RfqProcessor and set it listening for incoming RFQs
-        RfqProcessor rfqProc = new RfqProcessor(session, jssc);
-        rfqProc.startSocketListener();
+//        RfqProcessor rfqProc = new RfqProcessor(session, jssc);
+//        rfqProc.startSocketListener();
+        System.out.println("Listening for RFQ's");
+        jssc.start();
+        jssc.awaitTermination();
     }
 
 }
